5c5a8b102dd1f7e16621f2bd1ae44c26bce30260
St3v3n831 <yeiler8310@gmail.com>
2026-04-18 03:00:06 -0600

    Migrations and the reserve entity were created

---
+++ Models/Entities/Reservation.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace reservations_api.Models.Entities
+{
+    public class Reservation
+    {
+        public Guid Id { get; set; }
+
+        public DateOnly Date { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+
+        public TimeOnly EndTime { get; set; }
+
+        public Classroom Classroom { get; set; } = null!;
+
+    }
+}
